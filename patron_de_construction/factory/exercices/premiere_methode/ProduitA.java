fd373526aa2fb89cc89e3403c4f2d248c71eca00
herve7 <patricksonfack903@gmail.com>
2023-10-11 14:22:39 +0200

    Patron factory exercice premiere methode

---
+++ patron_de_construction/factory/exercices/premiere_methode/ProduitA.java
@@ -0,0 +1,6 @@
+package patron_de_construction.factory.exercices.premiere_methode;
+
+public abstract class ProduitA {
+    
+    public abstract void methodeA();
+}
---
+++ patron_de_construction/factory/exercices/premiere_methode/ProduitA2.java
@@ -0,0 +1,9 @@
+package patron_de_construction.factory.exercices.premiere_methode;
+
+public class ProduitA2 extends ProduitA{
+    
+    public void methodeA() {
+        System.out.println("Je suis un produit de type A2");
+        System.out.println("ProduitA2.methodeA();");
+     }
+}
