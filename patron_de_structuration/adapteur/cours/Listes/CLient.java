6a4f25915f0edd3e928fb958ad012a96937be392
herve7 <patricksonfack903@gmail.com>
2023-10-30 08:50:22 +0100

    modification effectué

---
+++ patron_de_structuration/adapteur/cours/Listes/CLient.java
@@ -0,0 +1,8 @@
+package patron_de_structuration.adapteur.cours.Listes;
+
+public class CLient {
+    
+    public static void main(String[] args){
+        System.out.println(" ");
+        }
+}
---
+++ patron_de_structuration/adapteur/exercices/listes/Client.java
@@ -0,0 +1,8 @@
+package patron_de_structuration.adapteur.exercices.listes;
+
+public class Client {
+    
+    public static void main(String[] args){
+    System.out.println(" ");
+    }
+}
